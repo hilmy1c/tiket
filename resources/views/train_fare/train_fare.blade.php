15748aba7165732d391f4b934d2924c35ac065a5
Muhammad Abdis Salim <muhammadabdissalim@gmail.com>
2018-02-05 22:35:29 +0700

    Mbenekno

--- resources/views/train_fare/train_fare.blade.php
+++ resources/views/train_fare/train_fare.blade.php
@@ -30,7 +30,7 @@
                                     {{ csrf_field() }}
                                     {{ method_field('DELETE') }}
 
-                                    <button type="submit" class="btn btn-danger">Delete</a>
+                                    <button type="submit" class="btn btn-danger">Delete</button>
                                 </form>
                             </td>
                         </tr>
